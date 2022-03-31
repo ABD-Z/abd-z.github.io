99f6904293e0b744571ef776130499dbd1d11886
Abdulmajid NASSER <aboud2008@gmail.com>
2022-04-01 00:40:46 +0200

    swap page via javascript + disable anchor when moving on new page displaying and enable it once the page is displayed

--- js/script.js
+++ js/script.js
@@ -24,13 +24,41 @@ function toggle_contact(social){
 	$("#black_layer").toggleClass("active");
 }
 
-function go_to(event, page){
-	/*$(".display").toggleClass("hide display");
-	$("#"+page).toggleClass("hide display");*/
-	console.log(event); console.log(page);
-	console.log(event.getAttribute("href"));
+var ignor_anchor = false;
 
+function go_to(event){
+	console.log(event);
+	var target = event.getAttribute("href");
+	var origin = ($(".display", "#menu")[0].getAttribute("href"));
+	if(target === origin){
+		console.log("noting");
+		return;
+	}else{
+		console.log("SWAP");
+		$(".display", "#menu").toggleClass("display")
+		$(event).addClass("display");
+		
+
+		
+		$(origin).toggleClass("display");
+		$(target).toggleClass("display");
+
+		ignor_anchor = true;
+		
+	}
+	
 }
+
+$(document).ready(function() {
+    $('a').on("click", function(e) {
+		if(ignor_anchor){
+			console.log(e.target);
+			e.preventDefault();
+			ignor_anchor = false;
+		}
+		
+    });
+});
 
 function splash_to_menu(){
     $("#logo").toggleClass("display");
