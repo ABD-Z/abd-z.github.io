88f82d673c12ffd189493068e44816d26d3ee684
Abdulmajid NASSER <aboud2008@gmail.com>
2022-04-05 16:56:31 +0200

    close contact box when clicking outside

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-(function() {
+(function () {
 	jQuery(window)
 
-	// Remove "loading" class once the page has fully loaded.
-		.on('load', function() {
+		// Remove "loading" class once the page has fully loaded.
+		.on('load', function () {
 			jQuery('body').removeClass('loading');
 		})
 
-	// Prevent scrolling on touch.
-		.on('touchmove', function() {
+		// Prevent scrolling on touch.
+		.on('touchmove', function () {
 			return false;
 		})
-	
-	// Fix scroll position on orientation change.
-		.on('orientationchange', function() {
+
+		// Fix scroll position on orientation change.
+		.on('orientationchange', function () {
 			jQuery('body').scrollTop(0);
 		});
 
@@ -21,54 +21,46 @@
 var ignor_anchor = false;
 var current_menu = "#home";
 
-function toggle_contact(social, social2){
-	$("#"+social).toggleClass("display");
-	$("#"+social2).removeClass("display");
-	//$("#black_layer").toggleClass("active");
+
+function toggle_contact(social, social2) {
+	window.event.stopPropagation();//stop propagate click detection to outside of button
+	$("#" + social).toggleClass("display");
+	$("#" + social2).removeClass("display");
+	$(document.body).on("click", function (e) { $("#" + social).removeClass("display"); }); //remove when click outside anywhere
 }
 
-
-
-function go_to(event){
-	console.log(event);
+function go_to(event) {
 	var target = event.getAttribute("href");
 	var origin = ($(".display", "#menu")[0].getAttribute("href"));
-	if(target === origin){
-		//console.log("noting");
+	if (target === origin) {
 		return;
-	}else{
-		//console.log("SWAP");
+	} else {
 		$(".display", "#menu").toggleClass("display")
 		$(event).addClass("display");
-		
 
-		
+
+
 		$(origin).toggleClass("display");
 		$(target).toggleClass("display");
 
 		ignor_anchor = true;
 		current_menu = target;
-		
 	}
-	
 }
 
-$(document).ready(function() {
-    $('a').on("click", function(e) {
-		if(ignor_anchor){
-			console.log(e.target);
+$(document).ready(function () {
+	$('a').on("click", function (e) {
+		if (ignor_anchor) {
 			e.preventDefault();
 			ignor_anchor = false;
 		}
-		
-    });
+	});
 });
 
-function splash_to_menu(){
-    $("#logo").toggleClass("display");
-    $("#caption").toggleClass("display");
+function splash_to_menu() {
+	$("#logo").toggleClass("display");
+	$("#caption").toggleClass("display");
 	$("#menu").toggleClass("display");
 	$("#name_title").toggleClass("display");
-    $(current_menu).toggleClass("display");
-  //  $("#logo").prop("onclick", null).off("click");
+	$(current_menu).toggleClass("display");
 }
